d897f8f65650bd58827a9109def1e49803397101
Dan McKeon <email": null>
2017-04-13 12:38:03 -0700

    create successful street view api request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,6 @@
 post '/searches' do
-  p params
+  original_img = open("https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}")
+  open('public/original_img/image22.jpg', 'wb') do |file|
+    file << original_img.read
+  end
 end
