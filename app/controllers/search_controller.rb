ef0b282e14096d0f9b737bd0be7e25e8d7faa4c5
Dan McKeon <email": null>
2017-04-13 13:55:50 -0700

    fix heading to orient toward address target and integrate address input with api request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
+require 'uri'
+require 'open-uri'
+
 post '/searches' do
-  original_img = open("https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}")
-  open('public/original_img/image22.jpg', 'wb') do |file|
+  address = URI.escape(params[:address])
+  original_img = open("https://maps.googleapis.com/maps/api/streetview?size=600x300&location=#{address}&pitch=-0.76&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}")
+  open('public/original_img/image55.jpg', 'wb') do |file|
     file << original_img.read
   end
 end
