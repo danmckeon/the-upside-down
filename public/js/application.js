5e2356244f645b32834726564acf1b31bf06bf79
Dan McKeon <email": null>
2017-04-13 15:04:19 -0700

    build street view search based on query string

--- public/js/application.js
+++ public/js/application.js
@@ -2,10 +2,14 @@ $(document).ready(function() {
   $('#address-input').on('submit', function(e) {
     e.preventDefault();
     var query = $('#address-input').serialize();
-    var saveImage = $.ajax({
+    var getImage = $.ajax({
       url: '/searches',
       type: 'POST',
       data: query
+    });
+    getImage.done(function(r) {
+      $('#address-input').find("input[type=text]").val('');
+      $('#original-img').html(r);
     });
   });
 });
